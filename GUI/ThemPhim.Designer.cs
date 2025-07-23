4dbe4da3c264a95b1fbc116326ee64df3a6dc462
DESKTOP-PKFG1AR\Dell inspiron <tanqui932013@gmail.com>
2025-07-23 19:10:26 +0700

    Tạo form

---
+++ GUI/ThemPhim.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace GUI
+{
+    partial class ThemPhim
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(73, 63);
+            label1.Name = "label1";
+            label1.Size = new Size(145, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Phim đã thêm";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(82, 438);
+            label2.Name = "label2";
+            label2.Size = new Size(68, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Mã phim";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(82, 491);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Tên phim";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(82, 545);
+            label4.Name = "label4";
+            label4.Size = new Size(62, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Thể loại";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(82, 592);
+            label5.Name = "label5";
+            label5.Size = new Size(81, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Thời lượng";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(613, 438);
+            label6.Name = "label6";
+            label6.Size = new Size(76, 20);
+            label6.TabIndex = 5;
+            label6.Text = "Tình trạng";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(613, 491);
+            label7.Name = "label7";
+            label7.Size = new Size(69, 20);
+            label7.TabIndex = 6;
+            label7.Text = "Sức chứa";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(613, 545);
+            label8.Name = "label8";
+            label8.Size = new Size(50, 20);
+            label8.TabIndex = 7;
+            label8.Text = "Giá vé";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(0, 192, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(245, 654);
+            button1.Name = "button1";
+            button1.Size = new Size(98, 36);
+            button1.TabIndex = 8;
+            button1.Text = "Thêm";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(0, 192, 0);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(741, 654);
+            button2.Name = "button2";
+            button2.Size = new Size(100, 36);
+            button2.TabIndex = 9;
+            button2.Text = "Làm mới";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(0, 192, 0);
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Location = new Point(582, 654);
+            button3.Name = "button3";
+            button3.Size = new Size(100, 36);
+            button3.TabIndex = 10;
+            button3.Text = "Xoá";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(0, 192, 0);
+            button4.ForeColor = SystemColors.ButtonHighlight;
+            button4.Location = new Point(419, 654);
+            button4.Name = "button4";
+            button4.Size = new Size(100, 36);
+            button4.TabIndex = 11;
+            button4.Text = "Cập nhật";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(0, 192, 0);
+            button5.ForeColor = SystemColors.ButtonHighlight;
+            button5.Location = new Point(883, 654);
+            button5.Name = "button5";
+            button5.Size = new Size(97, 36);
+            button5.TabIndex = 12;
+            button5.Text = "Thêm phim";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(205, 484);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(197, 27);
+            textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(206, 431);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(197, 27);
+            textBox2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(205, 538);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(197, 27);
+            textBox3.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(206, 585);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(196, 27);
+            textBox4.TabIndex = 16;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(741, 435);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(197, 27);
+            textBox5.TabIndex = 17;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(741, 484);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(197, 27);
+            textBox6.TabIndex = 18;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(741, 538);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(197, 27);
+            textBox7.TabIndex = 19;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(73, 94);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(1029, 312);
+            dataGridView1.TabIndex = 20;
+            // 
+            // ThemPhim
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1165, 716);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "ThemPhim";
+            Text = "ThemPhim";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private DataGridView dataGridView1;
+    }
+}
--- GUI/obj/Debug/net8.0-windows/GUI.AssemblyInfo.cs
+++ GUI/obj/Debug/net8.0-windows/GUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0b0137ae6d3fe28dea7c4380bc812a40c8e85f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4db0a8b12d9d1cdfeeadde16a7aee4da71d9002")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
