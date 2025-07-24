c53906d11b4bc66f502e47b0d5c964ec749c78b9
DESKTOP-PKFG1AR\Dell inspiron <tanqui932013@gmail.com>
2025-07-24 12:21:10 +0700

    ..

--- DTO_Model/obj/Debug/net8.0/DTO_Model.AssemblyInfo.cs
+++ DTO_Model/obj/Debug/net8.0/DTO_Model.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DTO_Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea19422dd1a1d1ecb1479d3105c4c4244f153d79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b2fead0481a0013e9dd834e2d00c41c8b0c16c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DTO_Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DTO_Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
